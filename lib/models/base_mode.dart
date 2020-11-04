9f6dc7a86dd41791775ceb8a8fa371c8233be725
BananaNeil <neil+1@countable.us>
2020-11-04 17:13:43 +0700

    fixup - basemodes are pre cached

--- lib/models/base_mode.dart
+++ lib/models/base_mode.dart
@@ -1,34 +1,77 @@
 import 'package:json_api/document.dart';
+import 'package:app/client.dart';
 import 'dart:convert';
 
 class BaseMode {
+  Map<String, dynamic> images;
+  num brightness;
+  num saturation;
   String name;
   num number;
   num page;
+  num hue;
   num id;
 
   BaseMode({
+    this.saturation,
+    this.brightness,
+    this.images,
     this.number,
     this.page,
     this.name,
+    this.hue,
     this.id,
   });
 
+  String get thumbnailPath => (images['club'] ?? {})['thumb'];
+  String get imagePath => (images['club'] ?? {})['medium'];
+  String get thumbnail => "${Client.host}${thumbnailPath}";
+  String get image => "${Client.host}${imagePath}";
+
+  String get trailImagePath => (images['trail'] ?? {})['medium'];
+  String get trailImage => "${Client.host}${trailImagePath}";
+
+  num getValue(param) {
+    return {
+      'brightness': brightness,
+      'saturation': saturation,
+      'hue': hue,
+    }[param];
+  }
+
   Map<String, dynamic> toMap() {
     return {
+      'brightness': brightness,
+      'saturation': saturation,
+      'images': images,
       'number': number,
       'page': page,
       'name': name,
+      'hue': hue,
       'id': id,
     } as Map;
+  }
+
+  ResourceObject toResource() {
+    return ResourceObject('base_mode', id.toString(), attributes: toMap(), relationships: {});
+  }
+
+  static String toJson(List<BaseMode> modes) {
+    return jsonEncode({
+      'data': modes.map((mode) => mode.toResource()).toList(),
+    });
   }
 
   factory BaseMode.fromMap(Map<String, dynamic> body) {
     var json = body;
     return BaseMode(
+      saturation: json['saturation'],
+      brightness: json['brightness'],
+      images: json['images'] ?? {},
       number: json['number'],
       page: json['page'],
       name: json['name'],
+      hue: json['hue'],
       id: json['id'],
     );
   }
